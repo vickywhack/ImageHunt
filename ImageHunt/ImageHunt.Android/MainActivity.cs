9de545af96e7c6b6889ad2ac7853d3b2f57de0f3
vikash_sahu <vikash13061992@gmail.com>
2021-07-19 03:28:08 +0530

    App : Android Support added

---
+++ ImageHunt/ImageHunt.Android/MainActivity.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Unity.Lifetime;
+using ImageHunt.Interfaces;
+using ImageHunt.Droid.Services;
+using ImageHunt.Register;
+
+namespace ImageHunt.Droid
+{
+    [Activity(Label = "ImageHunt", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+
+            Bootstrapper.RegisterPlatformDependency<IToastService, ToastService>(new SingletonLifetimeManager());
+            FFImageLoading.Forms.Platform.CachedImageRenderer.Init(true);
+
+            LoadApplication(new App());
+        }
+    }
+}
---
+++ ImageHunt/ImageHunt.Android/Services/ToastService.cs
@@ -0,0 +1,23 @@
+﻿using Android.Widget;
+using ImageHunt.Interfaces;
+using Xamarin.Forms;
+
+namespace ImageHunt.Droid.Services
+{
+    internal class ToastService : IToastService
+    {
+        private static Toast _toastInstance;
+
+        public void ShowToast(string message, bool isLongToast = false)
+        {
+            var toastLength = isLongToast ? ToastLength.Long : ToastLength.Short;
+
+            Device.BeginInvokeOnMainThread(() =>
+            {
+                _toastInstance?.Cancel();
+                _toastInstance = Toast.MakeText(Android.App.Application.Context, message, toastLength);
+                _toastInstance?.Show();
+            });
+        }
+    }
+}
